04f363295f18f21a004d90969acba11c8740846b
srajan-tibrewal <42686312+srajan-tibrewal@users.noreply.github.com>
2018-10-12 02:03:42 +0530

    Update Conditional Plots.py

--- Conditional Plots.py
+++ Conditional Plots.py
@@ -1,49 +1,32 @@
-
-# coding: utf-8
-
-# In[1]:
-
+#Code for implementing conditional plots
+# dataset encoding: utf-8
 
 import pandas as pd
 
 
-# In[4]:
-
-
+# import dataset and choose columns, display dataframe
 titanic = pd.read_csv("train.csv")
 titanic = titanic[["Survived","Pclass","Sex","Age","SibSp","Parch","Fare","Embarked"]]
 titanic = titanic.dropna()                   
-
-
-# In[5]:
-
-
 titanic.head()
 
 
-# In[6]:
+# Import plotting libraries
 
 
 import seaborn as sns 
 import matplotlib.pyplot as plt
 
 
-# In[9]:
+# Code for different kinds of conditional plots made. Best viewed in jupyter notebook
 
 
 sns.set_style("darkgrid")
 sns.distplot(titanic["Age"])
 plt.show()
 
-
-# In[10]:
-
-
 sns.kdeplot(titanic["Age"],shade = True)
 plt.xlabel("Age")
-
-
-# In[11]:
 
 
 sns.set_style("white")
@@ -51,27 +34,15 @@ sns.kdeplot(titanic["Age"],shade = True)
 plt.xlabel("Age")
 sns.despine(left = True, bottom = True)
 
-
-# In[13]:
-
-
 g = sns.FacetGrid(titanic, col = "Pclass",size=6)
 g.map(sns.kdeplot, "Age", shade = True)
 sns.despine(left = True,bottom = True)
 plt.show()
 
-
-# In[14]:
-
-
 g = sns.FacetGrid(titanic, col="Survived", row="Pclass")
 g.map(sns.kdeplot, "Age", shade=True)
 sns.despine(left=True, bottom=True)
 plt.show()
-
-
-# In[17]:
-
 
 g = sns.FacetGrid(titanic, col="Survived", row="Pclass",hue='Sex',size = 3)
 g.map(sns.kdeplot, "Age", shade=True)
